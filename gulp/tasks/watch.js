506cc595a252129c168dbaf021909da4327be3cb
Aslan <berezg86@gmail.com>
2019-10-13 08:51:46 +0300

    Добавил import reset.scss, исправил ошибку пути в watch для файла common.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ module.exports = function() {
     $.gulp.task('watch',function(){
         $.gulp.watch('src/pug/**/*.pug',$.gulp.series('pug'));
         $.gulp.watch('src/static/css/**/*.scss',$.gulp.series('sass'));
-        $.gulp.watch('src/static/js/main.js',$.gulp.series('scripts'));
+        $.gulp.watch('src/static/js/*.js',$.gulp.series('scripts'));
         $.gulp.watch('src/static/img/*',$.gulp.series('img'));
         $.gulp.watch('src/static/fonts/*',$.gulp.series('fonts'));
         $.gulp.watch('src/static/svg/*',$.gulp.series('svg'));
