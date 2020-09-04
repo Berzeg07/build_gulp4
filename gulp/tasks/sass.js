d7e66454d164d9d35de981c30bf0161cf2e2259d
Aslan <berezg86@gmail.com>
2020-09-04 14:56:00 +0300

    Подключил импорт файл с примером подключения шрифтов, исправил ошибку автопрефиксера

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,15 +2,15 @@ module.exports = function () {
     $.gulp.task('sass', function () {
         return $.gulp.src('src/static/css/main.scss')
             .pipe($.gp.sourcemaps.init())
-            .pipe($.gp.autoprefixer({
-                browsers: ['last 10 versions']
-            }))
             .on("error", $.gp.notify.onError({
                 message: "Error: <%= error.message %>",
                 title: "style"
             }))
             // .pipe($.gp.csso())
             .pipe($.gp.sass({ outputStyle: 'expanded' }))
+            .pipe($.gp.autoprefixer({
+                browsers: ['last 10 versions']
+            }))
             // .pipe($.gp.sourcemaps.write('./'))
             .pipe($.gulp.dest('build/css/'))
             // Минифицированная версия
